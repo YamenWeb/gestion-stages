8ab94509b9ecd2d77c08bb61af2804f6ac8db151
yamen <masmoudiyamen@gmail.com>
2020-04-28 13:26:39 +0100

    - mise en page de la page login - redirection des utilisateurs apres l'authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Etudiant;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,5 +34,24 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/login_success", name="login_success")
+     */
+    public function postLoginRedirectAction()
+    {
+        // usually you'll want to make sure the user is authenticated first
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // returns your User object, or null if the user is not authenticated
+        // use inline documentation to tell your editor your exact User class
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+        if ($user instanceof Etudiant) {
+            return $this->redirectToRoute("front");
+        } else if ($user instanceof User) {
+            return $this->redirectToRoute("admin");
+        }
     }
 }
